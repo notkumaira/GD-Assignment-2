21a0e631055910193b4c494129d75b3ef1aa5283
notkumaira <131270292+notkumaira@users.noreply.github.com>
2026-04-18 23:57:28 +0200

    StartScene Fix
    This reverts commit 2d71888753a3fb115854763275b07be3fd3a8536.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,30 +6,13 @@ public class UIManager : MonoBehaviour
 {
     public Button StartButton;
     public GameObject StartPanel;
-
-    public Button PauseButton;
-    public Button ResumeButton;
-    public GameObject PausePanel;
     void Start()
     {
         StartButton.onClick.AddListener(HidePanel);
-        PausePanel.SetActive(false);
-        ResumeButton.onClick.AddListener(HidePausePanel);
-        PauseButton.onClick.AddListener(DisplayPausePanel);
     }
 
     private void HidePanel()
     {
         StartPanel.SetActive(false);
-    }
-
-    private void HidePausePanel()
-    {
-        PausePanel.SetActive(false);
-    }
-
-    private void DisplayPausePanel()
-    {
-        PausePanel.SetActive(true);
     }
 }
