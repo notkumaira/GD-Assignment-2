7398c39e6e6998d72b3ff1ea3ae90d39223c51ae
divs69 <divya.seetal@gmail.com>
2026-04-20 15:31:01 +0200

    movement and the house layout with the completed and uncompleted levels

---
+++ Assets/Scripts/PlayerMovementNEW.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementNEW : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Animator anim;
+    private SpriteRenderer sprite; 
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        var keyboard = Keyboard.current;
+
+        if (keyboard != null)
+        {
+            
+            if (keyboard.dKey.isPressed) movement.x += 1;
+            if (keyboard.aKey.isPressed) movement.x -= 1;
+            if (keyboard.wKey.isPressed) movement.y += 1;
+            if (keyboard.sKey.isPressed) movement.y -= 1;
+
+           
+            transform.position += movement.normalized * speed * Time.deltaTime;
+
+            
+            
+            // 4. Update Animator Parameters
+            if (anim != null)
+            {
+                bool isMoving = movement.magnitude > 0;
+                
+                // tells the Animator to switch between Idle and Move states
+                anim.SetBool("moving", isMoving);
+
+                // tell the Blend Tree which direction to play
+                anim.SetFloat("moveX", movement.x);
+                anim.SetFloat("moveY", movement.y);
+            }
+        }
+    }
+}
